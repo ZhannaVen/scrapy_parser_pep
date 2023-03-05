e859a36e752653731c337dac3df0b36517e0b5bb
Zhanna Ventsenostseva <zhanna.ventsenostseva@gmail.com>
2023-03-05 16:52:46 +0300

    new spider

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,26 @@
+import scrapy
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        for pep_link in response.css('#numerical-index td a::attr(href)'):
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        number, name = (
+            response.css('h1.page-title::text').get().split(' – ')
+        )
+        data = {
+            'number': int(number.split()[1]),
+            'name': name,
+            'status': response.css(
+                'dt:contains("Status") + dd abbr::text'
+            ).get()
+        }
+        yield PepParseItem(data)
+
